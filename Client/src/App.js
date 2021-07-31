76eca3350c1bf9c6d7aa61ceb3a2d98ef9a39561
prabalp <prabalpanda2008@gmail.com>
2021-07-31 22:04:49 +0530

    signup

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,12 +3,18 @@ import Login from "./Components/Login";
 import Navbar from "./Components/Navbar";
 import Stage1 from "./Components/Designer/Stage1";
 import Stage2 from "./Components/Designer/Stage2";
+import SignupA from "./Components/Designer/Signup/SignupA";
+import SignupB from "./Components/Designer/Signup/SignupB";
+import SignupC from "./Components/Designer/Signup/SignupC";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Login />
+      <SignupC />
+      {/* <SignupB /> */}
+      {/* <SignupA /> */}
+      {/* <Login /> */}
       {/* <Stage1 /> */}
       {/* <Stage2 /> */}
     </div>
