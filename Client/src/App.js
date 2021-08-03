c59c44fe7897871939f7ac802562497541b4e272
prabalp <prabalpanda2008@gmail.com>
2021-08-03 18:34:31 +0530

    multiRangeSlider_attempt

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,6 +6,7 @@ import Stage2 from "./Components/Designer/Stage2";
 import SignupA from "./Components/Designer/Signup/SignupA";
 import SignupB from "./Components/Designer/Signup/SignupB";
 import SignupC from "./Components/Designer/Signup/SignupC";
+import Slider from "./Components/Designer/Slider/Slider";
 
 function App() {
   return (
@@ -16,7 +17,12 @@ function App() {
       {/* <SignupB /> */}
       {/* <SignupA /> */}
       {/* <Login /> */}
-      <Stage1 />
+      {/* <Stage1 /> */}
+      <Slider
+        min={0}
+        max={1000}
+        onChange={({ min, max }) => console.log(`min = ${min}, max = ${max}`)}
+      />
     </div>
   );
 }
