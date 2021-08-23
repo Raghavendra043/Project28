4dc7c6ac59fc4067988ac041eceffb0459fc6260
prabalp <prabalpanda2008@gmail.com>
2021-08-23 13:20:06 +0530

    sidenavbar

--- Client/src/Components/Designer/Dashboard/Main.js
+++ Client/src/Components/Designer/Dashboard/Main.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Brief from "./Brief";
 import Expt from "./Expt";
+import LeftNav from "./LeftNav";
 import Proj from "./Proj";
-import Updates from "./Updates";
+import TopNav from "./TopNav";
+import CurrentState from "./CurrentState";
 import Upload from "./Upload";
+import Updates from "./Updates";
+import Chatbox from "./Chatbox";
 
 function Main() {
   return (
@@ -12,44 +16,58 @@ function Main() {
         display: "flex",
       }}
     >
+      <LeftNav />
       <div
         style={{
           display: "flex",
           flexDirection: "column",
         }}
       >
-        <div>
-          <Proj />
+        <TopNav />
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <div>
+            <div>
+              <Proj />
+            </div>
+            <div>
+              <Expt />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Brief />
+            </div>
+            <div>
+              <Upload />
+            </div>
+          </div>
+          <div>
+            <div>
+              <CurrentState />
+            </div>
+            <div>
+              <CurrentState />
+            </div>
+            <div>
+              <CurrentState />
+            </div>
+            <div>
+              <CurrentState />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Updates />
+            </div>
+            <div>
+              <Chatbox />
+            </div>
+          </div>
         </div>
-        <div>
-          <Expt />
-        </div>
-      </div>
-      <div>
-        <div>
-          <Brief />
-        </div>
-        <div>
-          <Upload />
-        </div>
-      </div>
-      <div>
-        <div>
-          <Updates />
-        </div>
-        <div>
-          <Updates />
-        </div>
-        <div>
-          <Updates />
-        </div>
-        <div>
-          <Updates />
-        </div>
-      </div>
-      <div>
-        <div></div>
-        <div></div>
       </div>
     </div>
   );
---
+++ Client/src/Components/Designer/Dashboard/TopNav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TopNav() {
+  return (
+    <div style={{ display: "flex", padding: 20 }}>
+      <div style={{ display: "flex", flexGrow: 1 }}>ha</div>
+      <div>ha</div>
+    </div>
+  );
+}
+
+export default TopNav;
