fcef18c208ea0e9433fb4cd3695572c31c6d7dea
Srieon <speedshashwat@gmail.com>
2021-10-04 12:29:17 +0530

    project tracker changes

--- Client/src/Components/Admin_new/ProjectTracker/TrackerParts/Utilites.js
+++ Client/src/Components/Admin_new/ProjectTracker/TrackerParts/Utilites.js
@@ -5,28 +5,40 @@ import { ReactComponent as Download } from './../../assets/download.svg';
 
 function Utilites() {
 
-    var modal = document.getElementById("projectModal");
+    
+    window.onclick = function (event) {
+        var modal = document.getElementById("projectModal");
+        var modal1 = document.getElementById("projectModalGive");
+        var modal2 = document.getElementById("projectModalView");
+            if (event.target == modal || event.target == modal1 || event.target == modal2) {
+                event.target.style.display = "none";
+            }
+    }
     
     const approveHandler = () => {
+        var modal = document.getElementById("projectModal");
             modal.style.display = "block";
     }
     
-    const closeHandler = () => {
-            modal.style.display = "none"
+    
+    
+    const giveHandler = () => {
+        var modal = document.getElementById("projectModalGive");
+        modal.style.display = "block";
     }
     
-    window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-    }
-
-    const downloadHandler = () => {
-        
-    }
-
     const viewHandler = () => {
-        
+        var modal = document.getElementById("projectModalView");
+        modal.style.display = "block";
+    }
+
+    const closeHandler = () => {
+        var modal = document.getElementById("projectModal");
+            modal.style.display = "none"
+        var modal = document.getElementById("projectModalView");
+            modal.style.display = "none"
+        var modal = document.getElementById("projectModalGive");
+            modal.style.display = "none"
     }
 
     return (
@@ -45,7 +57,7 @@ function Utilites() {
                                 <button onClick={viewHandler} className={classnames(styles.button,styles.hello)}>View FeedBack</button>
                             </div>
                             <div className={styles.button_cover}>
-                                <button onClick={downloadHandler} className={classnames(styles.button,styles.hello)}>Download</button>
+                                <button onClick={giveHandler} className={classnames(styles.button,styles.hello)}>Give Feedback</button>
                             </div>
                         </div>
                     </div>
@@ -65,6 +77,40 @@ function Utilites() {
                         <div className={styles.button_cover}>
                                 <button className={classnames(styles.button)}>No</button>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div id="projectModalView" class={styles.modal}>
+                <div class={styles.modal_content}>
+                    <span class={styles.close} onClick={closeHandler}>&times;</span>
+                    <div className={styles.title}>View Feedback</div>
+
+
+                    <div className={styles.feedback_box}>
+                        <input type='text' className={styles.feedback} name='view_feedback'/>
+                        <div className={styles.button_cover}>
+                                <button className={classnames(styles.button)}>Feedback</button>
+                        </div>
+                    </div>
+                    <div className={styles.confirm_cover}>
+                        <button className={classnames(styles.button,styles.confirm)}>Confirm</button>
+                    </div>
+                </div>
+            </div>
+            <div id="projectModalGive" class={styles.modal}>
+                <div class={styles.modal_content}>
+                    <span class={styles.close} onClick={closeHandler}>&times;</span>
+                    <div className={styles.title}>Give Feeback</div>
+
+
+                    <div className={styles.feedback_box}>
+                        <input type='text' className={styles.feedback} name='view_feedback'/>
+                        <div className={styles.button_cover}>
+                                <button className={classnames(styles.button)}>Feedback</button>
+                        </div>
+                    </div>
+                    <div className={styles.confirm_cover}>
+                        <button className={classnames(styles.button,styles.confirm)}>Confirm</button>
                     </div>
                 </div>
             </div>
