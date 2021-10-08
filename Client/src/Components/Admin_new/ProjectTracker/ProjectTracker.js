5fc9fec3695c27bcd2fc870fb9cbeebf3bef2da5
Srieon <speedshashwat@gmail.com>
2021-10-08 20:13:44 +0530

    style changes

--- Client/src/Components/Admin_new/ProjectTracker/ProjectTracker.js
+++ Client/src/Components/Admin_new/ProjectTracker/ProjectTracker.js
@@ -1,59 +1,95 @@
-import React from 'react'
-import styles from './ProjectTracker.module.css';
-import About from './TrackerParts/About';
+import React from "react";
+import styles from "./ProjectTracker.module.css";
+import About from "./TrackerParts/About";
 import { useForm } from "react-hooks-helper";
-import Utilites from './TrackerParts/Utilites';
-import { ChatEngine } from 'react-chat-engine';
-import { ChatEngineWrapper, ChatSocket, ChatList, ChatFeed, ChatSettings } from 'react-chat-engine'
-import { Col } from 'react-grid-system'
-import axios from 'axios';
+import Utilites from "./TrackerParts/Utilites";
+import { ChatEngine } from "react-chat-engine";
+import {
+  ChatEngineWrapper,
+  ChatSocket,
+  ChatList,
+  ChatFeed,
+  ChatSettings,
+} from "react-chat-engine";
+import { Col } from "react-grid-system";
+import axios from "axios";
 
-var PROJECT_ID = '68a08b56-b99d-4754-85e8-375c862bef48'
-var USER_NAME = 'Ragh'
+var PROJECT_ID = "68a08b56-b99d-4754-85e8-375c862bef48";
+var USER_NAME = "Ragh";
 
 function ProjectTracker() {
-    const dummy = {"endDate":"","projectType":"Design","designermail":"Designer@gmail.com","id":1,"projectSummary":"Sample Project","files":{"0":{"files":[]},"1":{"files":[],"feedback":[],"adminFirstApproval":false,"clientFeedback":[],"approved":false,"adminSecondApproval":false,"currentState":"Waiting for Files from Designers"},"2":{"clientFeedback":[],"adminSecondApproval":false,"adminFirstApproval":false,"files":[],"approved":false,"feedback":[]}},"startDate":"12-12-12","notifications":[],"title":"Sample","currentStage":1,"clientEmail":"sample@gmail.com"}
+  const dummy = {
+    endDate: "",
+    projectType: "Design",
+    designermail: "Designer@gmail.com",
+    id: 1,
+    projectSummary: "Sample Project",
+    files: {
+      0: { files: [] },
+      1: {
+        files: [],
+        feedback: [],
+        adminFirstApproval: false,
+        clientFeedback: [],
+        approved: false,
+        adminSecondApproval: false,
+        currentState: "Waiting for Files from Designers",
+      },
+      2: {
+        clientFeedback: [],
+        adminSecondApproval: false,
+        adminFirstApproval: false,
+        files: [],
+        approved: false,
+        feedback: [],
+      },
+    },
+    startDate: "12-12-12",
+    notifications: [],
+    title: "Sample",
+    currentStage: 1,
+    clientEmail: "sample@gmail.com",
+  };
 
-    //const [formData1, setForm] = useForm(dummy);
-    const [ formData, setForm] = useForm(dummy);
-    axios.get(`http://localhost:9000/projectracker`).then(res =>{
-        console.log(res.data);    
-    setForm(res.data);
-        
-    }).catch((err)=>{
-        console.log(err);
+  //const [formData1, setForm] = useForm(dummy);
+  const [formData, setForm] = useForm(dummy);
+  axios
+    .get(`http://localhost:9000/projectracker`)
+    .then((res) => {
+      console.log(res.data);
+      setForm(res.data);
     })
-    const props = { formData, setForm}
-    //const props1 = {dummy, setdummy}
-    
-    return (
-        <div>
-            <div className={styles.container}>
-                <div className={styles.about}>
-                    {formData  ?(
-                        <About {...props}/>
-                    ) : null}
-                    
-                </div>
-                <div className={styles.details}>
-                    <Utilites />
-                </div>  
-                <div >
-                <Col  style={{height:"90vh", width:"27vw"}} >
-                    <ChatEngineWrapper>
-                        <ChatSocket 
-                            projectID={PROJECT_ID}
-                            chatID = '59600'
-                            chatAccessKey = 'ca-e9987fb7-0adb-4df6-8f7f-75b06cdc59c9'
-                            senderUsername = {USER_NAME}
-                        />
-                         <ChatFeed />  
-                    </ChatEngineWrapper>
-                </Col>
-                </div>
-            </div>
+    .catch((err) => {
+      console.log(err);
+    });
+  const props = { formData, setForm };
+  //const props1 = {dummy, setdummy}
+
+  return (
+    <div>
+      <div className={styles.container}>
+        <div className={styles.about}>
+          {formData ? <About {...props} /> : null}
         </div>
-    )
+        <div className={styles.details}>
+          {formData ? <Utilites {...props} /> : null}
+        </div>
+        <div>
+          <Col style={{ height: "90vh", width: "27vw" }}>
+            <ChatEngineWrapper>
+              <ChatSocket
+                projectID={PROJECT_ID}
+                chatID="59600"
+                chatAccessKey="ca-e9987fb7-0adb-4df6-8f7f-75b06cdc59c9"
+                senderUsername={USER_NAME}
+              />
+              <ChatFeed />
+            </ChatEngineWrapper>
+          </Col>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProjectTracker
+export default ProjectTracker;
--- Client/src/Components/Designer/ProfileCompletion/Parts/First.js
+++ Client/src/Components/Designer/ProfileCompletion/Parts/First.js
@@ -2,15 +2,14 @@ import React, { useRef, useState } from "react";
 import e_style from "./../../Signup/SignUp.module.css";
 import styles from "./First.module.css";
 import { ReactComponent as Man } from "./../assets/Firstman.svg";
-import classnames from 'classnames';
+import classnames from "classnames";
 
 function First() {
-
-  const [grad,setGrad]= useState('false')
+  const [grad, setGrad] = useState("false");
 
   const handleGrad = () => {
     setGrad(!grad);
-  }
+  };
   var expanded = false;
   const showCheckboxes = () => {
     var check = document.getElementsByClassName(styles.checkboxes)[0];
@@ -18,13 +17,12 @@ function First() {
       expanded = true;
       check.classList.add(styles.show);
       check.classList.remove(styles.hide);
-    }
-    else {
+    } else {
       expanded = false;
       check.classList.remove(styles.show);
       check.classList.add(styles.hide);
     }
-  }
+  };
 
   return (
     <div className={e_style.container}>
@@ -42,56 +40,145 @@ function First() {
           <div className={e_style.data}>
             <div className={e_style.input_cover}>
               <div className={e_style.titles}>Choose your Specialization</div>
-              
+
               <div className={styles.multiselect}>
                 <div className={styles.selectBox} onClick={showCheckboxes}>
-                <input type='text' className={styles.profile_input} placeholder='Select Your Specializaiton' disabled/>
-                    <div clasNname={styles.overSelect}></div>
-                  </div>
-                <div className={styles.checkboxes}>
-                    <label for="one">
-                    <input type="checkbox" id="one" className={styles.checkme}/>First checkbox</label>
-                    <label for="two">
-                      <input type="checkbox" id="two" className={styles.checkme} />Second checkbox</label>
-                    <label for="three">
-                      <input type="checkbox" id="three" className={styles.checkme}/>Third checkbox</label>
-                  </div>
+                  <input
+                    type="text"
+                    className={styles.profile_input}
+                    placeholder="Select Your Specializaiton"
+                    disabled
+                  />
+                  <div clasNname={styles.overSelect}></div>
                 </div>
+                <div className={classnames(styles.hide, styles.checkboxes)}>
+                  <label for="one">
+                    <input
+                      type="checkbox"
+                      id="one"
+                      className={styles.checkme}
+                    />
+                    First checkbox
+                  </label>
+                  <label for="two">
+                    <input
+                      type="checkbox"
+                      id="two"
+                      className={styles.checkme}
+                    />
+                    Second checkbox
+                  </label>
+                  <label for="three">
+                    <input
+                      type="checkbox"
+                      id="three"
+                      className={styles.checkme}
+                    />
+                    Third checkbox
+                  </label>
+                </div>
+              </div>
             </div>
-            <div className={classnames(e_style.input_cover,styles.checkbox_cover)}>
+            <div
+              className={classnames(e_style.input_cover, styles.checkbox_cover)}
+            >
               <div className={styles.left}>
-                <div className={classnames(e_style.titles,styles.titles)}>Are you a student?</div>
+                <div className={classnames(e_style.titles, styles.titles)}>
+                  Are you a student?
+                </div>
                 <span className={styles.check}>
-                  <input type="checkbox" name='yes' checked={grad} onChange={handleGrad} classname={styles.checkbox} />
-                  <label for='yes'style={{fontSize:'2.5vh'}}>Yes</label>
+                  <input
+                    type="checkbox"
+                    name="yes"
+                    checked={grad}
+                    onChange={handleGrad}
+                    classname={styles.checkbox}
+                  />
+                  <label for="yes" style={{ fontSize: "2.5vh" }}>
+                    Yes
+                  </label>
                 </span>
                 <span className={styles.check}>
-                  <input type="checkbox" name='no' checked={!grad} onChange={handleGrad} classname={styles.checkbox}/>
-                  <label for='no' style={{fontSize:'2.5vh'}}>No</label>
+                  <input
+                    type="checkbox"
+                    name="no"
+                    checked={!grad}
+                    onChange={handleGrad}
+                    classname={styles.checkbox}
+                  />
+                  <label for="no" style={{ fontSize: "2.5vh" }}>
+                    No
+                  </label>
                 </span>
               </div>
               <div className={styles.right}>
-                <div className={classnames(e_style.titles,styles.titles)} style={{display:grad===true?"block":'none'}}>Year of Graduation</div>
-                <input type='text' placeholder='Eg. 2022' style={{display:grad===true?"block":'none'}} className={styles.input}/>
-                <div className={classnames(e_style.titles,styles.titles)} style={{display:grad===true?"none":'block'}}>Year of Experience</div>
-                <input type='text' placeholder='Eg. 5' style={{display:grad===true?"none":'block'}} className={styles.input}/>
+                <div
+                  className={classnames(e_style.titles, styles.titles)}
+                  style={{ display: grad === true ? "block" : "none" }}
+                >
+                  Year of Graduation
+                </div>
+                <input
+                  type="text"
+                  placeholder="Eg. 2022"
+                  style={{ display: grad === true ? "block" : "none" }}
+                  className={styles.input}
+                />
+                <div
+                  className={classnames(e_style.titles, styles.titles)}
+                  style={{ display: grad === true ? "none" : "block" }}
+                >
+                  Year of Experience
+                </div>
+                <input
+                  type="text"
+                  placeholder="Eg. 5"
+                  style={{ display: grad === true ? "none" : "block" }}
+                  className={styles.input}
+                />
               </div>
             </div>
 
             <div className={e_style.input_cover}>
-              <div className={classnames(e_style.titles,styles.titles)}>Work Experience</div>
+              <div className={classnames(e_style.titles, styles.titles)}>
+                Work Experience
+              </div>
               <div className={styles.work}>
                 <div className={styles.work_input}>
-                  <input type='text' className={classnames(styles.input,styles.work_left)} placeholder="Your designation"/>
-                  <input type='text' className={classnames(styles.input,styles.work_right)} placeholder="Your Experience here"/>
+                  <input
+                    type="text"
+                    className={classnames(styles.input, styles.work_left)}
+                    placeholder="Your designation"
+                  />
+                  <input
+                    type="text"
+                    className={classnames(styles.input, styles.work_right)}
+                    placeholder="Your Experience here"
+                  />
                 </div>
                 <div className={styles.work_input}>
-                  <input type='text' className={classnames(styles.input,styles.work_left)} placeholder="Your designation"/>
-                  <input type='text' className={classnames(styles.input,styles.work_right)} placeholder="Your Experience here"/>
+                  <input
+                    type="text"
+                    className={classnames(styles.input, styles.work_left)}
+                    placeholder="Your designation"
+                  />
+                  <input
+                    type="text"
+                    className={classnames(styles.input, styles.work_right)}
+                    placeholder="Your Experience here"
+                  />
                 </div>
                 <div className={styles.work_input}>
-                  <input type='text' className={classnames(styles.input,styles.work_left)} placeholder="Your designation"/>
-                  <input type='text' className={classnames(styles.input,styles.work_right)} placeholder="Your Experience here"/>
+                  <input
+                    type="text"
+                    className={classnames(styles.input, styles.work_left)}
+                    placeholder="Your designation"
+                  />
+                  <input
+                    type="text"
+                    className={classnames(styles.input, styles.work_right)}
+                    placeholder="Your Experience here"
+                  />
                 </div>
               </div>
             </div>
