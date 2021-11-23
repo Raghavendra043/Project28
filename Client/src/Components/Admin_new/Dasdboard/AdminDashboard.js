591982ac8ba813374442a82d114c702f889afe74
Srieon <speedshashwat@gmail.com>
2021-11-23 22:36:13 +0530

    admin dash

--- Client/src/Components/Admin_new/Dasdboard/AdminDashboard.js
+++ Client/src/Components/Admin_new/Dasdboard/AdminDashboard.js
@@ -1,16 +1,69 @@
-import React from 'react'
-import styles from './Dashboard.module.css';
+import React, { useState } from "react";
+import styles from "./Dashboard.module.css";
+import { getData } from "../../../firebasefunctions/firestore";
+import { BarWave } from "react-cssfx-loading";
 
 function AdminDashboard() {
-    return (
-        <div>
-            <div className={styles.container}>
-                <div className={styles.project_list}>
-                    Admin dashboard
-                </div>
-            </div>
-        </div>
-    )
+  const [designerList, setdesignerList] = useState();
+  if (!designerList) {
+    getData(`Designers`)
+      .then((project) => {
+        console.log(project);
+        setdesignerList(project);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  if (designerList) {
+    var element = document.getElementById("loading1");
+    if (element) {
+      element.style.display = "none";
+    }
+    var element1 = document.getElementById("screen1");
+    if (element1) {
+      element1.style.display = "block";
+    }
+  }
+
+  return (
+    <div>
+      <div
+        className={styles.container}
+        id="screen1"
+        style={{ display: "none" }}
+      >
+        <div className={styles.project_list}>Admin dashboard</div>
+
+        {designerList && (
+          <table className={styles.list}>
+            <tr className={styles.heading}>
+              <th>Email</th>
+              <th>Name</th>
+              <th>Phone Number</th>
+            </tr>
+            {designerList.map((project) => (
+              <tr className={styles.details}>
+                <td>{project.email}</td>
+                <td>{project.name}</td>
+                <td>{project.phone}</td>
+              </tr>
+            ))}
+          </table>
+        )}
+      </div>
+      <div
+        id="loading1"
+        style={{ position: "absolute", marginTop: "45vh", marginLeft: "47vw" }}
+      >
+        <BarWave width="50px" height="50px" color="#1ABAA9" />
+        <p style={{ marginTop: "5vh", marginLeft: "-3vw" }}>
+          Getting account Info
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default AdminDashboard
+export default AdminDashboard;
--- Client/src/firebasefunctions/firestore.js
+++ Client/src/firebasefunctions/firestore.js
@@ -1,81 +1,79 @@
 import { db } from "../firebase";
 
-export const addData = async (collection, doc,Data) =>{
-    try {
-        if(Data && collection){
-            if(doc && doc.length>0){
-                await db.collection(collection).doc(doc).set(
-                    Data
-                );    
-            } else {
-                await db.collection(collection).doc(doc).add(
-                    Data
-                    );
-            }
-            
-        }
-        return true;
-    } catch (error) {
-        console.log(error);
+export const addData = async (collection, doc, Data) => {
+  try {
+    if (Data && collection) {
+      if (doc && doc.length > 0) {
+        await db.collection(collection).doc(doc).set(Data);
+      } else {
+        await db.collection(collection).doc(doc).add(Data);
+      }
     }
-}
+    return true;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const getDocData = async(Collection, Doc)=>{
-    try {
-        var data = await db.collection(Collection).doc(Doc).get();
-        return data.data();
-    } catch (error) {
-        console.log(error);
-    }
-}
+export const getDocData = async (Collection, Doc) => {
+  try {
+    var data = await db.collection(Collection).doc(Doc).get();
+    return data.data();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const getData = async(Collection)=>{
-    try {
-        var data = await db.collection(Collection).get();
-        var Data = [];
-        data.forEach(element => {
-            Data.push(element.data());
-        });
-        return Data 
-    } catch (error) {
-        console.log(error);
-    }
-}
+export const getData = async (Collection) => {
+  try {
+    var data = await db.collection(Collection).get();
+    var Data = [];
+    data.forEach((element) => {
+      Data.push(element.data());
+    });
+    return Data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const search =  async (collection , property, element )=>{
-    try {
-        let projectDetails;
-        const Data = await getData(collection);
-        for(const project of Data){
-            if(project[property] ==  element){
-                projectDetails = project;
-                break;
-            }
-        }
-        if(!projectDetails){
-            projectDetails = "f";
-        }
-        //console.log("from search : ", projectDetails);
-        return projectDetails;
-    } catch(err){
-        console.log(err);
+export const search = async (collection, property, element) => {
+  try {
+    let projectDetails;
+    const Data = await getData(collection);
+    for (const project of Data) {
+      if (project[property] == element) {
+        projectDetails = project;
+        break;
+      }
     }
-}
+    if (!projectDetails) {
+      projectDetails = "f";
+    }
+    //console.log("from search : ", projectDetails);
+    return projectDetails;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export const Update = async (collection, doc, data)=>{
-    try {
-        const Data = await getDocData(collection, doc);
-        let finalData;
-        if(Data){
-            finalData = {
-                ...Data, 
-                ...data
-            }
-        }
-        db.collection(collection).doc(doc).update(finalData).then(function() {
-            console.log("Frank food updated");
-          });
-    } catch(err){
-        console.log(err);
+export const Update = async (collection, doc, data) => {
+  try {
+    const Data = await getDocData(collection, doc);
+    let finalData;
+    if (Data) {
+      finalData = {
+        ...Data,
+        ...data,
+      };
     }
-}
+    db.collection(collection)
+      .doc(doc)
+      .update(finalData)
+      .then(function () {
+        console.log("Frank food updated");
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
