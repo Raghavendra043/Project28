514ee4592b0d1e7fd673dfeeb4c2e31a3dabb5b0
Srieon <speedshashwat@gmail.com>
2022-01-06 18:13:45 +0530

    scroll and deli

--- Client/src/Components/Admin_new/ProjectTracker/ProjectTracker.js
+++ Client/src/Components/Admin_new/ProjectTracker/ProjectTracker.js
@@ -4,7 +4,7 @@ import About from "./TrackerParts/About";
 import { useForm } from "react-hooks-helper";
 import Utilites from "./TrackerParts/Utilites";
 import { ChatEngine } from "react-chat-engine";
-import {addData, getDocData} from '../../../firebasefunctions/firestore'
+import { addData, getDocData } from "../../../firebasefunctions/firestore";
 import {
   ChatEngineWrapper,
   ChatSocket,
@@ -19,7 +19,7 @@ var PROJECT_ID = "68a08b56-b99d-4754-85e8-375c862bef48";
 var USER_NAME = "Ragh";
 
 function ProjectTracker({}) {
-  const title = '1';
+  const title = "1";
   const dummy = {
     endDate: "",
     projectType: "Design",
@@ -27,12 +27,12 @@ function ProjectTracker({}) {
     id: 1,
     projectSummary: "Sample Project",
     files: {
-      '0': { files: [] },
-      '1': {
-        files:[],
-        clientFiles :[],
-        adminFiles:[],
-        designerFiles:[],
+      0: { files: [] },
+      1: {
+        files: [],
+        clientFiles: [],
+        adminFiles: [],
+        designerFiles: [],
         feedback: [],
         adminApproval: false,
         clientApproval: false,
@@ -40,11 +40,11 @@ function ProjectTracker({}) {
         approved: false,
         currentState: "Waiting for Files from Designers",
       },
-      '2': {
-        files:[],
-        clientFiles :[],
-        adminFiles:[],
-        designerFiles:[],
+      2: {
+        files: [],
+        clientFiles: [],
+        adminFiles: [],
+        designerFiles: [],
         feedback: [],
         adminApproval: false,
         clientApproval: false,
@@ -62,48 +62,50 @@ function ProjectTracker({}) {
 
   //const [formData1, setForm] = useForm(dummy);
   const [formData, setForm] = useState(null);
-  if(!formData){
-    getDocData('Projects', title).then((project)=>{
-      console.log(project);
-      setForm(project);
-
-    }).catch((err)=>{
-      console.log(err);
-    })
+  if (!formData) {
+    getDocData("Projects", title)
+      .then((project) => {
+        console.log(project);
+        setForm(project);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-  
-  const print=  ()=>{
+
+  const print = () => {
     console.log("printed");
-  }
+  };
 
-  const props = { formData, setForm , print};
+  const props = { formData, setForm, print };
 
   return (
     <div>
-      {formData ? (<div className={styles.container}>
-        <div className={styles.about}>
-          {formData ? <About {...props} /> : null}
+      {formData ? (
+        <div className={styles.container}>
+          <div className={styles.about}>
+            {formData ? <About {...props} /> : null}
+          </div>
+          <div className={styles.details}>
+            {formData ? <Utilites {...props} /> : null}
+          </div>
+          <div>
+            <Col style={{ height: "90vh", width: "27vw" }}>
+              <ChatEngineWrapper>
+                <ChatSocket
+                  projectID={PROJECT_ID}
+                  chatID="59600"
+                  chatAccessKey="ca-e9987fb7-0adb-4df6-8f7f-75b06cdc59c9"
+                  senderUsername={USER_NAME}
+                />
+                <ChatFeed />
+              </ChatEngineWrapper>
+            </Col>
+          </div>
         </div>
-        <div className={styles.details}>
-          {formData ? <Utilites {...props} /> : null}
-        </div>
-        <div>
-          <Col style={{ height: "90vh", width: "27vw" }}>
-            <ChatEngineWrapper>
-              <ChatSocket
-                projectID={PROJECT_ID}
-                chatID="59600"
-                chatAccessKey="ca-e9987fb7-0adb-4df6-8f7f-75b06cdc59c9"
-                senderUsername={USER_NAME}
-              />
-              <ChatFeed />
-            </ChatEngineWrapper>
-          </Col>
-        </div>
-      </div>) : (
-        <div>fuck off</div>
+      ) : (
+        <div>Wait</div>
       )}
-
     </div>
   );
 }
--- Client/src/Components/Admin_new/ProjectTracker/TrackerParts/Utilites.js
+++ Client/src/Components/Admin_new/ProjectTracker/TrackerParts/Utilites.js
@@ -2,13 +2,13 @@
 import React from "react";
 import styles from "./Utilites.module.css";
 import classnames from "classnames";
-import {Approve, Reject} from '../../../../firebasefunctions/firestore'
+import { Approve, Reject } from "../../../../firebasefunctions/firestore";
 import App from "../../../../App";
 import { useHistory } from "react-router-dom";
 
-function Utilites({ formData, setForm , print}) {
+function Utilites({ formData, setForm, print }) {
   const history = useHistory();
-  const current  = formData.currentStage;
+  const current = formData.currentStage;
 
   const stages = [...Array(formData.currentStage).keys()];
   // window.onclick = function (event) {
@@ -33,19 +33,29 @@ function Utilites({ formData, setForm , print}) {
     modal.style.display = "block";
   };
 
-  const Approve1 = async ()=>{
+  const Approve1 = async () => {
     console.log("came here finally");
-    history.push('/feedback', {title:formData.title, id:formData.currentStage, from:'admin', request:1});
+    history.push("/feedback", {
+      title: formData.title,
+      id: formData.currentStage,
+      from: "admin",
+      request: 1,
+    });
     //const project = await Approve(formData);
     //setForm(project);
     //window.location.reload();
-  }
-  const Reject1 =async ()=>{
-    console.log('rejected');
+  };
+  const Reject1 = async () => {
+    console.log("rejected");
     await Reject(formData);
-    history.push('/feedback', {title:formData.title, id:formData.currentStage, from:'admin', request:0});
+    history.push("/feedback", {
+      title: formData.title,
+      id: formData.currentStage,
+      from: "admin",
+      request: 0,
+    });
     //window.location.reload();
-  }
+  };
 
   const giveHandler = () => {
     var modal = document.getElementById("projectModalGive");
@@ -75,64 +85,107 @@ function Utilites({ formData, setForm , print}) {
       <div className={styles.feed}>
         <div className={styles.title}>FEED</div>
 
-        <div>
-          {stages.map((data, key)=>{
-            return <div> {data+1}
-            {formData && formData.files[`${data+1}`]['adminFiles'].map((element, key)=>(
+        <div className={styles.list}>
+          {stages.map((data, key) => {
+            return (
+              <div>
+                {" "}
+                {data + 1}
+                {formData &&
+                  formData.files[`${data + 1}`]["adminFiles"].map(
+                    (element, key) => (
+                      <div className={styles.box}>
+                        <div className={styles.name}>{element.name}</div>
 
-              <div className={styles.box}>
-              <div className={styles.name}>
-                {element.name}
-              </div>
-  
-              <div className={styles.action}>
-                <div className={styles.button_cover}>
-                  {((key+1) === formData.files[`${data+1}`]['adminFiles'].length && 
-                    formData['files'][`${data+1}`]['adminApproval'] === false && formData['files'][`${data+1}`]['designerFiles'].length ===0
-                  ) && (
-                    <>
-                    <a href={element.url}><button onClick={Approve1} className={classnames(styles.button, styles.hello)}>File</button></a>
-                    <button onClick={approveHandler}
-                      // onclick={print}
-                      className={classnames(styles.button, styles.hello)}
-                    >
-                      Approve
-                    </button></>
+                        <div className={styles.action}>
+                          <div className={styles.button_cover}>
+                            {key + 1 ===
+                              formData.files[`${data + 1}`]["adminFiles"]
+                                .length &&
+                              formData["files"][`${data + 1}`][
+                                "adminApproval"
+                              ] === false &&
+                              formData["files"][`${data + 1}`]["designerFiles"]
+                                .length === 0 && (
+                                <>
+                                  <a href={element.url}>
+                                    <button
+                                      onClick={Approve1}
+                                      className={classnames(
+                                        styles.button,
+                                        styles.hello
+                                      )}
+                                    >
+                                      File
+                                    </button>
+                                  </a>
+                                  <button
+                                    onClick={approveHandler}
+                                    // onclick={print}
+                                    className={classnames(
+                                      styles.button,
+                                      styles.hello
+                                    )}
+                                  >
+                                    Approve
+                                  </button>
+                                </>
+                              )}
+                          </div>
+                          <div className={styles.button_cover}>
+                            {(formData["files"][`${data + 1}`][
+                              "adminApproval"
+                            ] === "rejected" ||
+                              formData["files"][`${data + 1}`][
+                                "adminApproval"
+                              ] === true ||
+                              formData["files"][`${data + 1}`]["designerFiles"]
+                                .length > 0 ||
+                              key + 1 <
+                                formData.files[`${data + 1}`]["adminFiles"]
+                                  .length) && (
+                              // key+1 < formData.files[current]['adminFiles'].length
+                              <button
+                                onClick={viewHandler}
+                                className={classnames(
+                                  styles.button,
+                                  styles.hello
+                                )}
+                              >
+                                View FeedBack / view File
+                              </button>
+                            )}
+                          </div>
+                          <div className={styles.button_cover}>
+                            {key + 1 ===
+                              formData.files[`${data + 1}`]["adminFiles"]
+                                .length &&
+                              formData["files"][`${data + 1}`][
+                                "adminApproval"
+                              ] === false &&
+                              formData["files"][`${data + 1}`]["designerFiles"]
+                                .length === 0 && (
+                                <button
+                                  onClick={giveHandler}
+                                  className={classnames(
+                                    styles.button,
+                                    styles.hello
+                                  )}
+                                >
+                                  Give Feedback
+                                </button>
+                              )}
+                          </div>
+                        </div>
+                      </div>
+                    )
                   )}
-                </div>
-                <div className={styles.button_cover}>
-                  {(formData['files'][`${data+1}`]['adminApproval'] === "rejected" ||
-                    formData['files'][`${data+1}`]['adminApproval'] === true || formData['files'][`${data+1}`]['designerFiles'].length > 0 || (key+1) < formData.files[`${data+1}`]['adminFiles'].length
-                    // key+1 < formData.files[current]['adminFiles'].length
-                  ) && (
-                    <button
-                      onClick={viewHandler}
-                      className={classnames(styles.button, styles.hello)}
-                    >
-                      View FeedBack / view File
-                    </button>
-                  )}
-                </div>
-                <div className={styles.button_cover}>
-                  {(key+1 === formData.files[`${data+1}`]['adminFiles'].length &&
-                    formData['files'][`${data+1}`]['adminApproval'] === false && formData['files'][`${data+1}`]['designerFiles'].length ===0
-                  ) && (<button
-                    onClick={giveHandler}
-                    className={classnames(styles.button, styles.hello)}
-                  >
-                    Give Feedback
-                  </button>)}
-                  
-                </div>
               </div>
-            </div>
-            ))}
-            </div>
+            );
           })}
-           
+
           {/* end of the map func */}
         </div>
-
 
         {/* <div className={styles.list}>
           <div className={styles.box}>
@@ -186,14 +239,14 @@ function Utilites({ formData, setForm , print}) {
 
           <div className={styles.actions}>
             <div className={styles.button_cover}>
-              <button onClick={Approve1} className={classnames(styles.button)}
-                
-              >Ye1s</button>
+              <button onClick={Approve1} className={classnames(styles.button)}>
+                Ye1s
+              </button>
             </div>
             <div className={styles.button_cover}>
-              <button className={classnames(styles.button)}
-                onClick={Reject1}
-              >No</button>
+              <button className={classnames(styles.button)} onClick={Reject1}>
+                No
+              </button>
             </div>
           </div>
         </div>
