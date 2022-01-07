61b63191a79f588fa2f3868d8d6c8b97eba0ed63
Srieon <speedshashwat@gmail.com>
2022-01-07 14:39:36 +0530

    notif added

--- Client/src/Components/Navbar/Navbar3.js
+++ Client/src/Components/Navbar/Navbar3.js
@@ -1,26 +1,49 @@
 import React from "react";
 import "./Navbar.css";
+import styles from "./Navbar.component.css";
+
 import logo from "./../../assets/logo.svg";
 import icon from "./../../assets/user.svg";
 import { useHistory } from "react-router-dom";
+import data from "./data.json";
 
 function Navbar3() {
   const history = useHistory();
+  const viewHandler = () => {
+    var modal = document.getElementById("projectModalNotif");
+    modal.style.display = "block";
+  };
+
+  const closeHandler = () => {
+    var modal = document.getElementById("projectModalNotif");
+    modal.style.display = "none";
+  };
   return (
     <div className="navbar">
       <div className="part1">
         <div className="navLogo">
-          <button className="nav-logo-button"><img src={logo} alt="logo" width='100vw'
-            onClick={()=>{
+          <button className="nav-logo-button">
+            <img
+              src={logo}
+              alt="logo"
+              width="100vw"
+              onClick={() => {
                 history.push("/");
-            }}
-          
-          /></button>
+              }}
+            />
+          </button>
         </div>
-        <div className="navA"><button>Designer List</button></div>
-        <div className="navA"><button>Client List</button></div>
-        <div className="navA"><button>Project</button></div>
+        <div className="navA">
+          <button>Designer List</button>
+        </div>
+        <div className="navA">
+          <button>Client List</button>
+        </div>
+        <div className="navA">
+          <button>Project</button>
+        </div>
       </div>
+
       <div className="toggle-button">
         <span class="bar"></span>
         <span class="bar"></span>
@@ -28,18 +51,42 @@ function Navbar3() {
       </div>
 
       <div className="part2">
-        {/* <div className="navB"><button>Notifications</button></div> */}
+        <div className="navB">
+          <button onClick={viewHandler}>Notifications</button>
+        </div>
         <div className="navE">
           <button
-            onClick={()=>{
-                alert("Are you sure you want to Logout ? ");
-                sessionStorage.clear();
-                
+            onClick={() => {
+              alert("Are you sure you want to Logout ? ");
+              sessionStorage.clear();
+
               history.push("/");
             }}
           >
             <img src={icon} alt="userIcon" width="22px" height="24px" />
           </button>
+        </div>
+      </div>
+      <div id="projectModalNotif" className="modal">
+        <div classname="modal_content">
+          <span className="close" onClick={closeHandler}>
+            &times;
+          </span>
+          <div className="title">Notifications</div>
+          <div className="notif_content">
+            {data &&
+              data.map((items, key) => {
+                return (
+                  <div
+                    className={
+                      (items.id === 1 ? "admin" : "client") + " notifs"
+                    }
+                  >
+                    {items.data}
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </div>
     </div>
