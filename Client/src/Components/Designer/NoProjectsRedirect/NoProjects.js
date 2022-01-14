6c6b60593a4228319d95121021a9a096df26ab4a
Srieon <speedshashwat@gmail.com>
2022-01-14 14:57:25 +0530

    changes

--- Client/src/Components/Designer/NoProjectsRedirect/NoProjects.js
+++ Client/src/Components/Designer/NoProjectsRedirect/NoProjects.js
@@ -63,78 +63,77 @@ function NoProjects({ x, formData }) {
       {/* <div className={styles.Sidebar}>
                 <Navbar/>
             </div> */}
-      <div className={styles.Redirect}>
-        <div className={styles.RedirectNested}>
-          <div className={styles.header}>You dont have any Projects Yet</div>
-          <div className={styles.svg}>
-            <Man />
-          </div>
-          <div className={styles.subheader}>Project-28</div>
-          {x && x != 1 ? (
-            <>
-              Please Complete Your Profile to take up projects
-              <button
-                style={{
-                  border: "none",
-                  color: "white",
-                  background: "#19BBB9",
-                  borderRadius: "5px",
-                }}
-                onClick={() => {
-                  history.push("/designer/complete", {
-                    email: formData.designerEmail,
-                  });
-                }}
-              >
-                Complete
-              </button>
-            </>
-          ) : (
-            <></>
-          )}
-          {x == 1 ? (
-            <>
-              You have been assigned A project
-              <button
-                style={{
-                  border: "none",
-                  color: "white",
-                  background: "#19BBB9",
-                  borderRadius: "5px",
-                }}
-                onClick={() => {}}
-              >
-                Download Project Details
-              </button>
-              <button
-                style={{
-                  border: "none",
-                  color: "white",
-                  background: "#19BBB9",
-                  borderRadius: "5px",
-                  marginTop: "3vh",
-                }}
-                onClick={Approve}
-              >
-                Approve
-              </button>
-              <button
-                style={{
-                  border: "none",
-                  color: "white",
-                  background: "#19BBB9",
-                  borderRadius: "5px",
-                  marginTop: "3vh",
-                }}
-                onClick={Disapprove}
-              >
-                DisApprove
-              </button>
-            </>
-          ) : (
-            <></>
-          )}
+
+      <div className={styles.RedirectNested}>
+        <div className={styles.header}>You dont have any Projects Yet</div>
+        <div className={styles.svg}>
+          <Man />
         </div>
+        <div className={styles.subheader}>Project-28</div>
+        {x && x != 1 ? (
+          <>
+            Please Complete Your Profile to take up projects
+            <button
+              style={{
+                border: "none",
+                color: "white",
+                background: "#19BBB9",
+                borderRadius: "5px",
+              }}
+              onClick={() => {
+                history.push("/designer/complete", {
+                  email: formData.designerEmail,
+                });
+              }}
+            >
+              Complete
+            </button>
+          </>
+        ) : (
+          <></>
+        )}
+        {x == 1 ? (
+          <>
+            You have been assigned A project
+            <button
+              style={{
+                border: "none",
+                color: "white",
+                background: "#19BBB9",
+                borderRadius: "5px",
+              }}
+              onClick={() => {}}
+            >
+              Download Project Details
+            </button>
+            <button
+              style={{
+                border: "none",
+                color: "white",
+                background: "#19BBB9",
+                borderRadius: "5px",
+                marginTop: "3vh",
+              }}
+              onClick={Approve}
+            >
+              Approve
+            </button>
+            <button
+              style={{
+                border: "none",
+                color: "white",
+                background: "#19BBB9",
+                borderRadius: "5px",
+                marginTop: "3vh",
+              }}
+              onClick={Disapprove}
+            >
+              DisApprove
+            </button>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
