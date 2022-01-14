7f13670b92192c9af0a968ad45d07f1fcbec2fba
Srieon <speedshashwat@gmail.com>
2022-01-14 14:47:34 +0530

    changes

--- Client/src/Components/Designer/NoProjectsRedirect/NoProjects.js
+++ Client/src/Components/Designer/NoProjectsRedirect/NoProjects.js
@@ -1,36 +1,46 @@
-import React from 'react'
-import Navbar from '../Dashboard new/Dashboard_Parts/Navbar/Navbar'
-import styles from './NoProjects.module.css'
-import {ReactComponent as  Man} from './man.svg'
-import { useHistory } from 'react-router-dom'
+import React from "react";
+import Navbar from "../Dashboard new/Dashboard_Parts/Navbar/Navbar";
+import styles from "./NoProjects.module.css";
+import { ReactComponent as Man } from "./man.svg";
+import { useHistory } from "react-router-dom";
 
-function NoProjects({x, email}) {
-    const history = useHistory();
-    return (
-        <div>
-            {/* <div className={styles.Sidebar}>
+function NoProjects({ x, email }) {
+  const history = useHistory();
+  return (
+    <div>
+      {/* <div className={styles.Sidebar}>
                 <Navbar/>
             </div> */}
-            <div className={styles.Redirect}>
-                <div className={styles.RedirectNested}>
-                <div className={styles.header}>Overall Performance {`&`} Account Details</div>
-                <div className={styles.svg}><Man/></div>
-                <div className={styles.subheader}>Priyanshu nigga hot bish got a fish in a dish with good meat to eat what a treat</div>
-                { x ? (
-                    <>Please Complete Your Profile<button
-                    onClick={()=>{
-                      history.push("/designer/complete", {email:email});
-                    }}
-                  >Complete</button></>
-                ) : (
-                    <></>
-                )
-
-                }
-                </div>
-                </div>
+      <div className={styles.Redirect}>
+        <div className={styles.RedirectNested}>
+          <div className={styles.header}>
+            Overall Performance {`&`} Account Details
+          </div>
+          <div className={styles.svg}>
+            <Man />
+          </div>
+          <div className={styles.subheader}>
+            Priyanshu nigga hot bish got a fish in a dish with good meat to eat
+            what a treat
+          </div>
+          {x ? (
+            <>
+              Please Complete Your Profile
+              <button
+                onClick={() => {
+                  history.push("/designer/complete", { email: email });
+                }}
+              >
+                Complete
+              </button>
+            </>
+          ) : (
+            <></>
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default NoProjects
+export default NoProjects;
